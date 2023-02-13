1171bd2d281528e9143903eb25e273ec5e706aa0
mecpalmo <mecpalmo@gmail.com>
2023-02-13 15:25:39 +0100

    cleanup4

--- game_logic.py
+++ game_logic.py
@@ -1,1 +1,25 @@
+class Field:
+    def __init__(self, _x, _y, _value):
+        self.x
+        self.y
+        self.value = _value
 
+    def count_Flags(self):
+        ...
+
+    def count_Closed_Unknown(self):
+        ...
+  
+
+class Solution:
+    def __init__(self):
+        self.field_guesses = []
+
+    def generate_Solution(self, _x, _y, _bomb):
+        self.field_guesses.append(Field_guess(_x, _y, _bomb))
+
+class Field_guess:
+    def __init__(self, _x, _y, _bomb):
+        self.x = _x
+        self.y = _y
+        self.bomb = _bomb #True or False
